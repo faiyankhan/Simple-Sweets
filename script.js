1111601e62ec37279abe2b0652b760184d3017c7
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-03-30 16:24:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const fadeout = () => {
+    setTimeout(() => {
+        const loader = document.querySelector(".preloader");
+        loader.classList.remove("preloader");
+    }, 2000); // 2000 milliseconds = 2 seconds
+}
+
+window.addEventListener("load", fadeout);
